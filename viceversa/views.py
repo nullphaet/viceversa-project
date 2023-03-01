9b52eb1d666eac2ed34e4ec549c52e6da798511c
Null Phaet <phaet@phaet.local>
2023-03-01 13:32:00 +0530

    added counter

--- viceversa/views.py
+++ viceversa/views.py
@@ -8,5 +8,5 @@ def home(request):
 def reverse(request):
 	user_text = request.GET['usertext']
 	reversed_text = user_text[::-1]
-	return render(request, 'reverse.html', {'user_text': user_text, 'reversed_text': reversed_text})
-	
+	words_count = len(user_text.split())
+	return render(request, 'reverse.html', {'user_text': user_text, 'reversed_text': reversed_text, 'words_count': words_count})
